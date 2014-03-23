42dbbe2a334efec41272a5cfa9074244eb01ea37
Naveen <kothamas@usc.edu>
2014-03-22 23:43:40 -0700

    comments removed

--- highfreqpairs.py
+++ highfreqpairs.py
@@ -32,35 +32,5 @@ for line in inputFile:
 			j += 1;
 			
 		i += 1;
-	'''
-	for word in wordList:
-		inputMap[uid].append(word);
-		if word in likesAndUIDsMap:
-			likesAndUIDsMap[word].append(uid);
-		else:
-			likesAndUIDsMap[word] = [uid];
-
-		if word in likesAndCountsMap:
-			likesAndCountsMap[word] += 1;
-		else:
-			likesAndCountsMap[word] = 1;
-			allLikes.append(word);
-i = 0;
-while(i < len(allLikes)):
-	j = i+1;
-	while(j < len(allLikes)):
-		like1 = allLikes[i];
-		like2 = allLikes[j];
-
-		word = like1+","+like2;
-		#likePairsAndCountsMap[word] = len(Set(likesAndUIDsMap[like1]).intersection(Set(likesAndUIDsMap[like2])));
-		likePairsAndCountsMap[word] = 0;
-		for entry in likesAndUIDsMap[like1]:
-			if entry in likesAndUIDsMap[like2]:
-				likePairsAndCountsMap[word] += 1;
-				print word+"\t"+str(likePairsAndCountsMap[word]);
-		j += 1;
-	i += 1;
-'''
 for entry in likePairsAndCountsMap:
 	print entry+"\t"+str(likePairsAndCountsMap[entry]);
